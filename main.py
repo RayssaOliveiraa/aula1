bd83753c49353d86c3817c3ed9341822172b1328
Rayssa Oliveira <87939812+RayssaOliveiraa@users.noreply.github.com>
2022-12-04 16:41:49 +0000

    Exercicio01contpares

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Cesar receba o melhor")
-print("caladinho !")
+cont=0
+for i in range(1,6):
+    nun1 = int(input(f"Informe um numero {i} inteiro: "))
+    if nun1 % 2 == 0:
+        cont+=1
+print(f"Você digitou {cont}, numeros pares")       
